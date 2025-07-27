38a3213144c60812ef7c9a69925ef90208e2a3e4
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2025-07-27 11:52:55 +0300

    Updates

--- characters.e785a7bb.js
+++ characters.e785a7bb.js
@@ -1,2 +1,2 @@
 const e=document.querySelector(".explore__box"),t=Array.from(e.children);for(let o=0;o<2;o++)t.forEach(t=>e.appendChild(t.cloneNode(!0)));e.style.gap="33px";const o=t[0].offsetWidth,r=t.length,n=o*r+33*(r-1);let a=0;!function t(){Math.abs(a-=1.5)>=n&&(a+=n),e.style.transform=`translateX(${a}px)`,requestAnimationFrame(t)}();
-//# sourceMappingURL=rick-and-morty.59e2108e.js.map
+//# sourceMappingURL=characters.e785a7bb.js.map
